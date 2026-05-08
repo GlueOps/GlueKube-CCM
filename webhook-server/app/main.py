798f00334941619ba0f1b8e699a8022b02b684f5
Bazmahou <bouissihamza6@gmail.com>
2026-05-08 08:41:21 +0000

    feat: add internal_traffic_policy to sync function for improved traffic management

--- webhook-server/app/main.py
+++ webhook-server/app/main.py
@@ -174,6 +174,7 @@ def sync(request: dict):
             ports=service_ports,
             type="ClusterIP",
             external_i_ps=public_ips+private_ips,
+            internal_traffic_policy="Local",
             ip_family_policy=service['spec'].get('ipFamilyPolicy'),
             session_affinity=service['spec'].get('sessionAffinity')
         )
